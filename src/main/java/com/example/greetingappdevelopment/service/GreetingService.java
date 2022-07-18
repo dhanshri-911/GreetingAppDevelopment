f7ba45f5e7edb15ad1a577d9ffe656d43ee7fd04
Dhanshri911 <dszingade911@gmail.com>
2022-07-19 00:51:51 +0530

    Added:UC3 Greeting Messages

--- src/main/java/com/example/greetingappdevelopment/service/GreetingService.java
+++ src/main/java/com/example/greetingappdevelopment/service/GreetingService.java
@@ -7,4 +7,7 @@ public class GreetingService {
     public String getmessage() {
         return "Hello World";
     }
+    public String sayHelloByName(String firstName, String lastName) {
+        return "Hello "+firstName+" "+lastName;
+    }
 }
